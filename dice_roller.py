6425930193bcefc5e3ae5d958f45ac23923a724b
kartik shah <78698460+kartik9807@users.noreply.github.com>
2021-05-21 14:09:20 +0530

    Add files via upload

---
+++ dice_roller.py
@@ -0,0 +1,33 @@
+import random #imported random module
+def dice():# making a function for dice rolling
+    while(True): # putting in loop
+        a = input("Press entry to roll the dice :) ")
+        if(a=="exit"):
+            exit(0)
+        dice = random.randint(1,6)
+        if(dice==1):
+            print("[     ]")
+            print("[  0  ]")
+            print("[     ]\n")
+        if(dice==2):
+            print("[0    ]")
+            print("[     ]")
+            print("[    0]\n")
+        if(dice==3):
+            print("[  0  ]")
+            print("[  0  ]")
+            print("[  0  ]\n")
+        if(dice==4):
+            print("[0   0]")
+            print("[     ]")
+            print("[0   0]\n")
+        if(dice==5):
+            print("[0   0]")
+            print("[  0  ]")
+            print("[0   0]\n")
+        if(dice==6):
+            print("[0   0]")
+            print("[0   0]")
+            print("[0   0]\n")
+            
+dice()
